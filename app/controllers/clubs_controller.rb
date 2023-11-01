77077ff094160ae11e7d8486c41f25837bc2c606
AlejaH1213 <alejajara2@gmail.com>
2023-11-01 11:00:28 -0700

    create functionality and testing

--- app/controllers/clubs_controller.rb
+++ app/controllers/clubs_controller.rb
@@ -3,4 +3,18 @@ class ClubsController < ApplicationController
         clubs = Club.all
         render json: clubs
     end
+
+    def create
+        club = Club.create(club_params)
+        if club.valid?
+            render json: club
+        else
+            render json: club.errors, status: 422
+        end
+    end
+    
+    private
+    def club_params
+        params.require(:club).permit(:name, :summary, :meeting_dates, :book_of_the_month, :book_of_the_month_picture)
+    end
 end
--- app/models/club.rb
+++ app/models/club.rb
@@ -1,4 +1,5 @@
 class Club < ApplicationRecord
     has_many :memberships
     has_many :users, through: :memberships
+    validates :name, :summary, :meeting_dates, :book_of_the_month, :book_of_the_month_picture, presence: true
 end
