c6d8315427e4af3ea94897daa2231ca22363156e
AlejaH1213 <alejajara2@gmail.com>
2023-11-07 13:26:23 -0800

    fixed seeds again and added method in controller so all the entries are in order

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,7 +21,7 @@ user2 = User.where(email: "mark.twain@example.com").first_or_create(
 
   club1 = Club.create(
     name: "Fantasy Book Club",
-    summary: "A club for fans of epic fantasy literature. Join us in exploring magical realms and heroic adventures.",
+    summary: "A club for fans of epic fantasy literature. Join us in exploring magical realms and heroic adventures filled with dragons and more.",
     meeting_dates: "1st and 3rd Fridays of each month",
     book_of_the_month: "The Fellowship of the Ring",
     book_of_the_month_picture: "https://media.istockphoto.com/id/522513933/photo/book-and-glowing-letters.jpg?s=1024x1024&w=is&k=20&c=8WfzoSTOdY66J5n4UUAdG-H3UbujWClS0Nxaq5Ai9l0="
@@ -29,7 +29,7 @@ user2 = User.where(email: "mark.twain@example.com").first_or_create(
 
   club2 = Club.create(
     name: "Non-Fiction Book Club",
-    summary: "Dive into the world of non-fiction literature. We explore topics ranging from history to science and self-help.",
+    summary: "Dive into the world of non-fiction literature. We explore topics ranging from history to science and self-help. Everyone is welcomed",
     meeting_dates: "2nd and 4th Thursdays of each month",
     book_of_the_month: "Sapiens: A Brief History of Humankind",
     book_of_the_month_picture: "https://media.istockphoto.com/id/1447533975/photo/sign-on-library-shelves-non-fiction.jpg?s=1024x1024&w=is&k=20&c=Up_RUch-zaLe_PLZok66IraOMo-ydMSHehWDxPAcPNg="
@@ -37,27 +37,38 @@ user2 = User.where(email: "mark.twain@example.com").first_or_create(
 
   club3 = Club.create(
     name: "Historical Book Club",
-    summary: "Travel back in time with us to explore different eras, events, and figures in history through literature.",
+    summary: "Travel back in time with us to explore different eras, events, and figures in history through literature. From the past to the present",
     meeting_dates: "1st and 3rd Wednesdays of each month",
     book_of_the_month: "All the Light We Cannot See",
     book_of_the_month_picture: "https://images.unsplash.com/photo-1461360370896-922624d12aa1?auto=format&fit=crop&q=80&w=2948&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   )
 
 
-  membership1 = user1.memberships.create(
+  membership1 = user1.memberships.create!(
     club: club1,
     name: 'first club'
   )
 
-  membership2 = user2.memberships.create(
+  membership2 = user2.memberships.create!(
     club: club2,
     name: 'second club'
   )
 
-  membership3 = user2.memberships.create(
+  membership3 = user2.memberships.create!(
     club: club3,
     name: 'third club'
   )
+
+p "user1 memberships", user1.memberships
+p "user2 memberships", user2.memberships
+p "user1 clubs", user1.clubs
+p "user2 clubs", user2.clubs
+p "club1 memberships", club1.memberships
+p "club2 memberships", club2.memberships
+p "club3 memberships", club3.memberships
+p "club1 users", club1.users
+p "club2 users", club2.users
+p "club3 users", club3.users
 
   
 
