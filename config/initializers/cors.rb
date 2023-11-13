39bc5df5473c4da9010a961c75ee6b97f1476dba
AlejaH1213 <alejajara2@gmail.com>
2023-11-13 13:06:11 -0800

    right cors url without http

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'https://bookclub-frontend.onrender.com'
+      origins 'bookclub-frontend.onrender.com'
       resource '*',
       headers: ["Authorization"],
       expose: ["Authorization"],
